2e329df5f4deaf0bf713435c1e875db5351b0746
lg <luiz.vargas@agpr5.com>
2025-10-30 16:51:50 -0300

    :sparkles: feat: Adicionando Web Analytics

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,12 +2,14 @@ import { ReactNode } from 'react';
 import { NextThemesProvider } from './next-themes';
 import { SonnerProvider } from './sonner';
 import { UseQueryProvider } from './use-query';
+import { Analytics } from '@vercel/analytics/next';
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <NextThemesProvider>
       <UseQueryProvider>
         <SonnerProvider />
+        <Analytics />
         {children}
       </UseQueryProvider>
     </NextThemesProvider>
